1722c59ec206f7cc6694469773450289291a49cd
Denys Almazov <almazovdenys@gmail.com>
2025-04-06 15:12:26 +0300

    init commit

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,50 @@
-const key = new Key();
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
 
+  public getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+
+  public getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean;
+  protected key: Key;
+  protected tenants: Person[];
+
+  constructor(key: Key) {
+    this.key = key;
+    this.tenants = [];
+  }
+
+  public comeIn(person: Person) {
+    this.tenants.push(person);
+  }
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (key === this.key) {
+      this.door = true;
+    }
+  }
+}
+
+const key = new Key();
 const house = new MyHouse(key);
 const person = new Person(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
-
 
 export {};
